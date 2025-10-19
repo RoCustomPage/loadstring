d5f4e160e82fe2b45fabf9f821bc76f2387ac8b3
RoCustom <wemoonwixs@gmail.com>
2025-10-19 15:30:15 +0300

    Update autofarm.lua

--- autofarm.lua
+++ autofarm.lua
@@ -1,8 +1,1 @@
-local player = game.Players.LocalPlayer
-local gui = Instance.new("ScreenGui", player.PlayerGui)
-local box = Instance.new("Frame", gui)
-box.Size = UDim2.new(0, 80, 0, 80)
-box.Position = UDim2.new(0.5, 0, 0.5, 0)
-box.AnchorPoint = Vector2.new(0.5, 0.5)
-box.BackgroundColor3 = Color3.new(0,0,0)
-Instance.new("UICorner", box).CornerRadius = UDim.new(0, 12)
+print(1)
